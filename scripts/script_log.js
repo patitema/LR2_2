c6c7e2a6d28c8e6c45dfeb3c0de82624fe821176
patitema <artemavhimovih@gmail.com>
2024-05-03 08:26:04 +0700

    back

--- scripts/script_log.js
+++ scripts/script_log.js
@@ -1,12 +1,18 @@
 document.getElementById('myForm').addEventListener('submit', function(e) {
   e.preventDefault(); // Останавливаем отправку формы
 
-  var login = document.getElementById('login').value;
-  var password = document.getElementById('password').value;
+  let login = document.getElementById('login').value;
+  let password = document.getElementById('password').value;
 
-  localStorage.setItem('SavedLogin', login);
-  localStorage.setItem('SavedPass', password);
+  let Tlogin = localStorage.getItem('SavedLogin');
+  let Tpassword = localStorage.getItem('SavedPass');
 
-  window.location.href = '/pages/Profile.html';
+  if(login == Tlogin && password == Tpassword ){
+    window.location.href = '/pages/Profile.html';
+  }
+  else{
+    alert("Неверный логин или пароль")
+  }
+
 
 });
